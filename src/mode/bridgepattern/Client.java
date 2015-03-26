4ff35d72177be881e965c491b237cfdd6ba657a1
= <=>
2015-03-26 15:22:27 +0800

    添加新改动文件

--- src/mode/bridgepattern/Client.java
+++ src/mode/bridgepattern/Client.java
@@ -1,16 +1,13 @@
 package bridgepattern;
 
+/*
 //抽象公司
 abstract class Corp {
 
-    /**
-     * 生成行为
-     */
+    //生成行为
     protected abstract void produce();
 
-    /**
-     * 销售行为
-     */
+    //销售行为
     protected abstract void sell();
 
     public void makeMoney() {
@@ -56,15 +53,88 @@ class ClothesCorp extends Corp {
     }
 
 }
+*/
+
+interface Product {
+
+    //生成行为
+    public void beProducted();
+
+    //销售行为
+    public void beSelled();
+
+}
+
+//生产房子
+class House implements Product {
+
+    @Override
+    public void beProducted() {
+        System.out.println("房子被生产出来");
+    }
+
+    @Override
+    public void beSelled() {
+        System.out.println("房子被销售");
+    }
+}
+
+//生产Ipod
+class IPod implements Product {
+
+    @Override
+    public void beProducted() {
+        System.out.println("IPod被生产出来");
+    }
+
+    @Override
+    public void beSelled() {
+        System.out.println("IPod被销售");
+    }
+}
+
+//公司类,公司生产产品
+abstract class Corp {
+    private Product product;
+
+    //聚合关系
+    public Corp(Product product) {
+        this.product = product;
+    }
+
+    //赚钱
+    public void makeMoney() {
+        product.beProducted();
+        product.beSelled();
+    }
+}
+
+//派生房地产公司
+class HouseCorp extends Corp {
+
+    public HouseCorp(Product product) {
+        super(product);
+    }
+}
+
+//派生服装公司
+class ClothesCorp extends Corp {
+
+    public ClothesCorp(Product product) {
+        super(product);
+    }
+}
 
 public class Client {
-    public static void main(String args[]){
-        System.out.println("看房地产公司怎么赚钱");
-        HouseCorp houseCorp=new HouseCorp();
+    public static void main(String args[]) {
+        //房地产公司盖房子
+        House house = new House();
+        HouseCorp houseCorp = new HouseCorp(house);
         houseCorp.makeMoney();
-        System.out.println("\n");
-        System.out.println("看服装公司怎么赚钱");
-        ClothesCorp clothesCorp=new ClothesCorp();
+
+        //服装公司生产ipod
+        IPod iPod = new IPod();
+        ClothesCorp clothesCorp = new ClothesCorp(iPod);
         clothesCorp.makeMoney();
     }
 }
